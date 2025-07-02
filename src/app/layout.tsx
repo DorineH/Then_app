60e1362a57e8786851f8cfbe4d83a2e0e1c1880e
DorineH <dorinematildehenry@gmail.com>
2025-07-02 17:06:40 +0200

    T-2 navigation composant + setup page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import BottomNavigation from '@/components/BottomNavigation'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -23,12 +13,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <link rel="manifest" href="/manifest.json" />
-      <meta name="theme-color" content="#C8A1E0" />
-      <link rel="apple-touch-icon" href="/icons/icon-128x128.png" />
+    <html lang="fr">
+      <body>
+        {/* Container principal avec padding bottom pour la nav */}
+        <main className="pb-16 min-h-screen">{children}</main>
 
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+        {/* Barre de navigation fixe */}
+        <BottomNavigation />
+      </body>
     </html>
   )
 }
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+export default function Profil() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600">Page profiles en cours de construction</h1>
+  )
+}
